6ce94649ea121fd180fd14820d56c29094d3c8e9
adityasuman2025 <adityasuman2025@gmail.com>
2020-10-02 11:02:52 +0530

    basic project setup done

---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import NotFound from './pages/NotFound';
+
+const Routes = () => (
+<BrowserRouter >
+    <Switch>
+        {/* <Route exact path="/" component={ LandingPage } /> */}
+
+        <Route path="*" component={ NotFound } />
+    </Switch>
+</BrowserRouter>
+);
+
+export default Routes;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NotFound(props) {
+    return <div className="redColored">Page not found</div>;
+}
+
+export default NotFound;
